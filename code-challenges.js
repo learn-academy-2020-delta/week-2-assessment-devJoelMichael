72bc375f7cb6dd58fa3e75077f9f9926869f0b90
LEARN Academy <devjoelmichael@protonmail.com>
2020-09-12 15:15:15 -0700

    completed W2 assessment

--- code-challenges.js
+++ code-challenges.js
@@ -12,8 +12,19 @@ var num2 = 0
 var num3 = -7
 // Expected output: "-7 is not divisble by three"
 
-
-
+// create a function that takes a number 
+const threeMe = (num) => {
+    if(num % 3 === 0) {
+// see if this number is divisible by three or not (if statment)
+        return `${ num } is divisible by three`
+    } else {
+// if it is not return something that lets them know
+        return `${ num } is NOT divisible by three`
+    }
+}
+console.log(threeMe(num1))
+console.log(threeMe(num2))
+console.log(threeMe(num3))
 
 
 // --------------------2) Create a function that takes in the randomNouns variable and returns an array with all the words capitalized.
@@ -21,7 +32,18 @@ var num3 = -7
 var randomNouns = ["streetlamp", "potato", "teeth", "conclusion", "nephew", "temperature", "database"]
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew", "Temperature", "Database"]
 
-
+const capNouns = (arr) => {
+    //create a variable to split array
+    //create a new empty array for the new array to be pushed into
+    var nounsNowCap = []
+//iterate through that array using and capitalize the first letter in every word
+    for(let i = 0; i < arr.length; i++) {
+        nounsNowCap.push(randomNouns[i].charAt(0).toUpperCase()+ randomNouns[i].slice(1))
+    }
+    //return the newly capitalized words into a new array because you don't want to modify original data.
+    return nounsNowCap
+}
+console.log(capNouns(randomNouns))
 
 
 
@@ -30,8 +52,13 @@ var randomNouns = ["streetlamp", "potato", "teeth", "conclusion", "nephew", "tem
 var mixedDataArray = [true, 8, "hello", 90, -8, null, 0, 46, 59, 107, "hey!"]
 // Expected output: [-8, 0, 8, 46, 59, 90, 107]
 
-
-
+// Create a function that finds only numbers
+const onlyNums = (arr) => {
+// return only numbers, remember typeof will help here
+    return arr.filter(value => typeof value === 'number')
+}
+// again do not forget to run mixedDataArray inside of your function
+console.log(onlyNums(mixedDataArray))
 
 
 // --------------------4) Create a function that takes in a string and logs the index of the first vowel.
@@ -41,25 +68,51 @@ var vowelTester1 = "learn"
 var vowelTester2 = "throw"
 // Expected output: 3
 
-
-
-
+// Make a function that takes a string and finds the index of the first vowel.
+const findVowel = (string) => {
+// figure out a way to seperate the word into just letters in the array
+    let word = string.split("")
+// then search through the letters till you hit your first vowel
+    let getThemVowels = word.filter(vowel => vowel === "a" || vowel === "e" || vowel  === "i" || vowel  === "o" || vowel  === "u") 
+    // create an array that stores the vowel 
+    let firstVowelInString = getThemVowels[0]
+    // make sure that it finds the index of the vowel and does not return just the vowel
+    let indexOfVowel = string.indexOf(firstVowelInString)
+    return indexOfVowel
+}
+console.log(findVowel(vowelTester1))
+console.log(findVowel(vowelTester2))
 
 
 // --------------------5) Create a function that takes three arguments - two numbers and a mathematical operation (+, -, *, /) and performs the given calculation. If the input tries to divide by 0, return: "Can't divide by 0!"
 
-
-
-
-
+const calculator = (num1, operator, num2) => {
+// create a way for it to check if its one of the 4 operators (probably if/else)
+    if(operator === "/") {
+        // make sure that if the second number is divided by 0 it will return "cant divide by 0!"
+        if(num2 === 0) {
+            return "Can't divide by 0!"
+        } else {
+            return num1 / num2
+        }    
+    } else if(operator === "+") {
+        return num1 + num2
+    } else if(operator === "-") {
+        return num1 - num2
+    } else if(operator === "*") {
+        return num1 * num2
+    } else {
+        return "Please input a number"
+    }
+}
 // Uncomment and use the following console logs to test your function
-// console.log(calculator(3, "*", 9))
+console.log(calculator(3, "*", 9))
 // Expected output: 27
 
-// console.log(calculator(16, "+", 3))
+console.log(calculator(16, "+", 3))
 // Expected output: 19
 
-// console.log(calculator(89, "/", 0))
+console.log(calculator(89, "/", 0))
 // Expected output: "Can't divide by 0!"
 
 
@@ -69,7 +122,29 @@ var vowelTester2 = "throw"
 var websites = ["codepen.io", "codecademy.com", "coursera.org", "codepen.io", "udemy.com", "pluralsight.com", "udacity.com", "sitepoint.com"]
 // Expected output: "codepen.io" "codepen.io"
 
+// Create a function that takes in the array of websites
+const newWebsite = (arr) => {
+// find a way to look through the array and find the end since .io is only at the end 
+    let ios = arr.filter(value => value.endsWith(".io")) 
+// return only the onlys that are .io
+    return ios 
+    //return ios.join(" ") if you wanted to make them into a string
+}
+
+console.log(newWebsite(websites))
 
 
+// --------------------7) STRETCH: Write a function that takes a number as an argument and uses a WHILE loop to count up to that number from 0.
 
-// --------------------6) STRETCH: Write a function that takes a number as an argument and uses a WHILE loop to count up to that number from 0.
+// Create a function that takes in a users number
+const countUp = (number) => {
+// declare the variable and use a while loop to generate the desired number
+    let a = 0;
+    while (a < number) {
+// make sure you have it count up from zero instead of down to zero
+        a++;
+        console.log(a)
+    }
+}
+
+console.log(10)
--- interview-questions.md
+++ interview-questions.md
@@ -7,81 +7,86 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. Why would you use the method `super()`?
 
-  Your answer:
+  Your answer: The super() lets us call out to the parent function/method.
 
-  Researched answer:
+  Researched answer: When used in a constructor, the super keyword appears alone and must be used before the this keyword is used. The super keyword can also be used to call functions on a parent object.
 
 
 
 2. What is the DOM? What is the virtual DOM?
 
-  Your answer:
+  Your answer: Something to do with the computers connection to the internet?
 
-  Researched answer:
+  Researched answer: The HTML DOM (Document Object Model)
+When a web page is loaded, the browser creates a Document Object Model of the page.
+- The virtual DOM (VDOM) is a programming concept where an ideal, or “virtual”, representation of a UI is kept in memory and synced with the “real” DOM by a library such as ReactDOM. This process is called reconciliation.
 
 
 
 3. What is a higher-order function?
 
-  Your answer:
+  Your answer: A higher order function is a function that uses .map() and .filter() to condense the code, and make it easier to use, instead of for loops. 
 
-  Researched answer:
+  Researched answer: In a nutshell, a Higher-order function is a function that may receive a function as an argument and can even return a function. Higher-order functions are just like regular functions with an added ability of receiving and returning other functions are arguments and output.
 
 
 
 4. What is JSX?
 
-  Your answer:
+  Your answer: JavaScript run in html through react.
 
-  Researched answer:
+  Researched answer: It is called JSX, and it is a syntax extension to JavaScript. We recommend using it with React to describe what the UI should look like. JSX may remind you of a template language, but it comes with the full power of JavaScript.
+
+JSX produces React “elements”. We will explore rendering them to the DOM in the next section. Below, you can find the basics of JSX necessary to get you started.
 
 
 
 5. What is React?
 
-  Your answer:
+  Your answer: React is a pre built app that allows small chunks of code to be built and then put together piece by piece.
 
-  Researched answer:
+  Researched answer: React is an open-source JavaScript library for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications.
 
 
 
 6. What is yarn? What file(s) are updated when you run `$ yarn`?
 
-  Your answer:
+  Your answer: yarn is the pre made assests stored inside of React. You get all the files updated that are inside the basic update, both in src and file.
 
-  Researched answer:
+  Researched answer: Yarn is a new package manager that replaces the existing workflow for the npm client or other package managers while remaining compatible with the npm registry. It has the same feature set as existing workflows while operating faster, more securely, and more reliably.
 
 
 
 7. What is localhost:3000? How do we use localhost:3000 in the development process?
 
-  Your answer:
+  Your answer: it is a preinstalled html page that lets us run our React application in real time, though it is not connected to the internet.
 
-  Researched answer:
+  Researched answer: localhost is a network address that points to your own computer. Navigating to localhost:3000 in your web browser sends a request to the Rails development server running locally on port 3000. However, this only works if your web browser is running on the same computer as the development server.
 
 
 
 8. What is an iterable in JavaScript? What data types are iterables? How can this information useful to you as a developer?
 
-  Your answer:
+  Your answer: I honestly do not know what this is and had to look it up.
 
-  Researched answer:
+  Researched answer: The iterable protocol allows JavaScript objects to define or customize their iteration behavior, such as what values are looped over in a for...of construct. Some built-in types are built-in iterables with a default iteration behavior, such as Array or Map , while other types (such as Object ) are not. 
+  - They would be helpful because it is less code to type and more efficiant?
 
 
 
 9. STRETCH: What is hoisting in JavaScript?
 
-  Your answer:
+  Your answer: Creating a function that can have multiple functions inside it?
 
-  Researched answer:
+  Researched answer: Hoisting is the JavaScript interpreter's action of moving all variable and function declarations to the top of the current scope. However, only the actual declarations are hoisted. Any assignments are left where they are. Therefore, our second example IIFE actually translates to the following code.
 
 
 
 10. STRETCH: What are closures in JavaScript?
 
-  Your answer:
+  Your answer: Is it the end of your JavaScript that closes out the info and sends it all to the html or app?
 
-  Researched answer:
+  Researched answer: A closure is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment). ... In JavaScript, closures are created every time a function is created, at function creation time.
 
 
 
